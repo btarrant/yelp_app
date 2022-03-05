4bb2526ce951e6f3eaac6b5fedaa66eb19ce5a5f
Brandon Tarrant <brandontarrant04@gmail.com>
2022-03-05 13:09:58 -0500

    first commit

---
+++ server.py
@@ -0,0 +1,12 @@
+import requests
+
+url = "https://api.yelp.com/v3/businesses/search"
+api_key = "oRBqX0o-AcuJCHdx28EySX3tlP-a3UV7ekmMNzxSztCAjiYoHVLhFdD0c-5mHHyp78vnUvpZM656N35XMOhSowikdb7g3HrASHe_gy9YVBFMnjY0Z-YxHQ4tu6AjYnYx"
+headers = {
+    "Authorization": "Bearer " + api_key
+}
+params = {
+    "location": "Altamonte Springs, FL"
+}
+response = requests.get(url, headers=headers, params=params)
+print(response.text)
