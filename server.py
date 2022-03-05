90b3c16de84f5abd3793268ed74a4fee23659409
Brandon Tarrant <brandontarrant04@gmail.com>
2022-03-05 14:04:20 -0500

    Hid key.

--- server.py
+++ server.py
@@ -1,15 +1,16 @@
 import requests
+import config
 
 url = "https://api.yelp.com/v3/businesses/search"
-api_key = "oRBqX0o-AcuJCHdx28EySX3tlP-a3UV7ekmMNzxSztCAjiYoHVLhFdD0c-5mHHyp78vnUvpZM656N35XMOhSowikdb7g3HrASHe_gy9YVBFMnjY0Z-YxHQ4tu6AjYnYx"
 headers = {
-    "Authorization": "Bearer " + api_key
+    "Authorization": "Bearer " + config.api_key
 }
 params = {
     "term": "Pizza",  # term = Keyword
-    "location": "Altamonte Springs, FL"
+    "location": "Orlando, FL"
 }
 response = requests.get(url, headers=headers, params=params)
 businesses = response.json()["businesses"]
-for business in businesses:
-    print(business["name"])
+names = [business["name"]
+         for business in businesses if business["rating"] > 3.5]
+print(names)
